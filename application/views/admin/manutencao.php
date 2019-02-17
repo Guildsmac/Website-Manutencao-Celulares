4edea72c7a5ec70227500fb55eaa1ec766016ffb
Gabriel S <gabrielssc.ti@gmail.com>
2019-02-17 20:51:31 -0300

    Fix

--- application/views/admin/manutencao.php
+++ application/views/admin/manutencao.php
@@ -25,75 +25,79 @@
                 <th>Excluir</th>
 			</tr>
         <?php
-            $formAttr = array('id' => 'loginForm');
-            echo form_open("index.php/admin/AdminManutencao/alter_data", $formAttr);
-            foreach($manutencao as $i){
+        if($manutencao!=null){
+        $formAttr = array('id' => 'loginForm');
+        echo form_open("index.php/admin/AdminManutencao/alter_data", $formAttr);
+        foreach ($manutencao
+
+        as $i){
+        ?>
+        <tr>
+            <th><?php echo $i->dtPedido ?></th>
+            <th><?php echo $i->dtEntrega != '' ? $i->dtEntrega : 'Ainda não entregue.' ?></th>
+            <th><?php
+                $formData = array(
+                    'name' => 'custo_' . $i->idManutencao,
+                    'class' => 'bota_uma_classe_lian',
+                    'value' => $i->custo,
+                    'style' => 'width:75'
+                );
+                echo form_input($formData);
+
                 ?>
-                <tr>
-                    <th><?php echo $i->dtPedido?></th>
-                    <th><?php echo $i->dtEntrega != '' ? $i->dtEntrega : 'Ainda não entregue.'?></th>
-                    <th><?php
-                        $formData = array(
-                            'name' => 'custo_' . $i->idManutencao,
-                            'class' => 'bota_uma_classe_lian',
-                            'value' => $i->custo,
-                            'style' => 'width:75'
-                        );
-                        echo form_input($formData);
+            </th>
+            <th><?php
+                $formData = array(
+                    'name' => 'nomeProb_' . $i->idManutencao,
+                    'class' => 'bota_uma_classe_lian',
+                    'value' => $i->problema,
+                    'style' => 'width:150'
 
-                        ?>
-                    </th>
-                    <th><?php
-                        $formData = array(
-                            'name' => 'nomeProb_' . $i->idManutencao,
-                            'class' => 'bota_uma_classe_lian',
-                            'value' => $i->problema,
-                            'style' => 'width:150'
+                );
+                echo form_input($formData);
+                ?>
+            </th>
+            <th><?php
+                $formData = array(
+                    'name' => 'descProb_' . $i->idManutencao,
+                    'class' => 'bota_uma_classe_lian',
+                    'value' => $i->descricaoProblema,
+                    'rows' => 2,
+                    'cols' => 30
+                );
+                echo form_textarea($formData);
+                ?>
+            </th>
+            <th>
+                <?php
+                $checkBoxData = array(
+                    'name' => 'entregue_' . $i->idManutencao,
+                    'checked' => $i->dtEntrega == '' ? FALSE : TRUE,
 
-                        );
-                        echo form_input($formData);
-                        ?>
-                    </th>
-                    <th><?php
-                        $formData = array(
-                            'name' => 'descProb_' . $i->idManutencao,
-                            'class' => 'bota_uma_classe_lian',
-                            'value' => $i->descricaoProblema,
-                            'rows' => 2,
-                            'cols' => 30
-                        );
-                        echo form_textarea($formData);
-                        ?>
-                    </th>
-                    <th>
-                        <?php
-                        $checkBoxData = array(
-                            'name' => 'entregue_' . $i->idManutencao,
-                            'checked' => $i->dtEntrega == '' ? FALSE : TRUE,
+                );
+                echo form_checkbox($checkBoxData);
+                ?>
+            </th>
+            <th>
+                <?php
+                $checkBoxData = array(
+                    'name' => 'excluir_' . $i->idManutencao,
+                    'checked' => FALSE,
 
-                        );
-                        echo form_checkbox($checkBoxData);
-                        ?>
-                    </th>
-                    <th>
-                        <?php
-                        $checkBoxData = array(
-                            'name' => 'excluir_' . $i->idManutencao,
-                            'checked' => FALSE,
+                );
+                echo form_checkbox($checkBoxData); ?>
+            </th>
 
-                        );
-                        echo form_checkbox($checkBoxData);?>
-                    </th>
-
-                    <?php
+            <?php
             }
             ?>
-	</table>
+    </table>
     <?php
-        $buttonData = array(
-            'class' => 'active'
-        );
-        echo form_submit('confirmButton', 'Confirmar Alterações', $buttonData);
-        echo form_close();
+    $buttonData = array(
+        'class' => 'active'
+    );
+    echo form_submit('confirmButton', 'Confirmar Alterações', $buttonData);
+    echo form_close();
+    }
     ?>
 </div>
