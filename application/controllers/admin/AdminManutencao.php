046b9b3f574e37436460525bdf199d50657d9bf6
Gabriel S <gabrielssc.ti@gmail.com>
2019-02-17 22:01:53 -0300

    Added edit and delete to manutencao

--- application/controllers/admin/AdminManutencao.php
+++ application/controllers/admin/AdminManutencao.php
@@ -11,7 +11,28 @@ class AdminManutencao extends CI_Controller
 {
 
     public function alter_data(){
-        var_dump($this->input->post());
+        $post_data = $this->input->post();
+        var_dump($post_data);
+        $manutencao = $this->db->get('manutencao')->result();
+        foreach($manutencao as $i){
+            $id = $i->idManutencao;
+            if(isset($post_data['excluir_' . $id])){
+                $this->db->where('idManutencao', $id);
+                $this->db->delete('manutencao');
+            }
+            else{
+                $updating_data=array(
+                    'custo' => $post_data['custo_' . $id],
+                    'problema' => $post_data['nomeProb_' . $id],
+                    'descricaoProblema' => $post_data['descProb_' . $id],
+                    'dtEntrega' => isset($post_data['entregue_' . $id]) ? date('d/m/y') : ''
+                );
+                $this->db->where('idManutencao', $id);
+                $this->db->update('manutencao', $updating_data);
+            }
+
+        }
+        redirect(base_url('index.php/admin/AdminManutencao'));
     }
 
 	public function index(){
