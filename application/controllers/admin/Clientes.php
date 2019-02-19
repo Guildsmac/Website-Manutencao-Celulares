99b3b9bf0c670f10bbaaf735f2aa8e9d89f86cb3
Gabriel S <gabrielssc.ti@gmail.com>
2019-02-19 12:08:22 -0300

    Terminado toda a página de Administração

--- application/controllers/admin/Clientes.php
+++ application/controllers/admin/Clientes.php
@@ -3,8 +3,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Clientes extends CI_Controller
 {
 
+    public function gerencia_clientes(){
+        $post_data = $this->input->post();
+        var_dump($post_data);
+        $usuarios = $this->db->get('usuario')->result();
+        foreach($usuarios as $i){
+            $id = $i->idUsuario;
+            if(isset($post_data['excluir_'.$id])){
+                $this->db->where('idUsuario', $id);
+                $this->db->delete('usuario');
+            }else{
+                $updating_data = array(
+                    'nome' => $post_data['nome_'.$id],
+                    'email' => $post_data['email_'.$id],
+                    'dtNascimento' => $post_data['dtnasc_'.$id],
+                    'endereco' => $post_data['endereco_'.$id],
+                    'numTelefone' => $post_data['telefone_'.$id],
+                    'cpf' => $post_data['cpf_'.$id],
+                    'permissionLevel' => isset($post_data['admin_'.$id]) ? 1 : 0
+                );
+                $this->db->where('idUsuario', $id);
+                $this->db->update('usuario', $updating_data);
+            }
+        }
+        redirecT(base_url('index.php/admin/clientes'));
+    }
+
 	public function index(){
-		$this->template->load('admin/admin_template', 'admin/clientes');
+        $this->load->helper('password_confirmer');
+        $usuarios = $this->db->get('usuario')->result();
+        $r = array('usuarios' => $usuarios);
+        password_confirmer($this, $this->session->userdata(), 'admin/clientes', $r);
 	}
 	
 }
--- application/views/admin/clientes.php
+++ application/views/admin/clientes.php
@@ -1,66 +1,129 @@
 <?php
-     defined('BASEPATH') OR exit('No direct scripts access allowed');
- ?>
+defined('BASEPATH') OR exit('No direct scripts access allowed');
+?>
 
 
 <div class="container">
-	<div class="header">
+    <div class="header">
         <a class="logo">Administração</a>
         <div class="header-right">
             <a class="active" href="http://localhost/manutencaodecelulares/index.php/admin/AdminHomepage">Início</a>
         </div>
-    </div> 
-		<h1>
-		 	Clientes
-		</h1>
+    </div>
+    <h1>
+        Clientes
+    </h1>
     <table class="table">
-		<tr>
+        <tr>
             <th>Nome</th>
-			<th>Email</th>
-			<th>Data de Nascimento</th>
-			<th>Endereço</th>
-			<th>Telefone</th>
-			<th>CPF</th> 
+            <th>Email</th>
+            <th>Data de Nascimento</th>
+            <th>Endereço</th>
+            <th>Telefone</th>
+            <th>CPF</th>
+            <th>Deletar</th>
+            <th>Administrador</th>
         </tr>
-        <tr>
-			<td>Lian</td>
-			<td>lianfsilva@hotmail.com</td>
-			<td>18/02/2001</td>
-			<td>Rua 11, Quadra L</td>
-            <td>91321626</td>
-            <td>123.456.789-10</td>
-        </tr>
-        <tr>
-			<td>Lian</td>
-			<td>lianfsilva@hotmail.com</td>
-			<td>18/02/2001</td>
-			<td>Rua 11, Quadra L</td>
-            <td>91321626</td>
-            <td>123.456.789-10</td>
-        </tr>
-        <tr>
-			<td>Lian</td>
-			<td>lianfsilva@hotmail.com</td>
-			<td>18/02/2001</td>
-			<td>Rua 11, Quadra L</td>
-            <td>91321626</td>
-            <td>123.456.789-10</td>
-        </tr>
-        <tr>
-			<td>Lian</td>
-			<td>lianfsilva@hotmail.com</td>
-			<td>18/02/2001</td>
-			<td>Rua 11, Quadra L</td>
-            <td>91321626</td>
-            <td>123.456.789-10</td>
-        </tr>
-        <tr>
-			<td>Lian</td>
-			<td>lianfsilva@hotmail.com</td>
-			<td>18/02/2001</td>
-			<td>Rua 11, Quadra L</td>
-            <td>91321626</td>
-            <td>123.456.789-10</td>
-        </tr>
-    </table> 
+        <?php
+        if (isset($usuarios)) {
+            echo form_open('index.php/admin/Clientes/gerencia_clientes');
+            foreach ($usuarios as $i) {
+                $id = $i->idUsuario;
+                ?>
+                <tr>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'nome_'.$id,
+                            'value' => $i->nome,
+
+                        );
+                        echo form_input($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'email_'.$id,
+                            'value' => $i->email,
+
+                        );
+                        echo form_input($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'dtnasc_'.$id,
+                            'value' => $i->dtNascimento,
+
+                        );
+                        echo form_input($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'endereco_'.$id,
+                            'value' => $i->endereco,
+
+                        );
+                        echo form_input($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'telefone_'.$id,
+                            'value' => $i->numTelefone,
+
+                        );
+                        echo form_input($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'cpf_'.$id,
+                            'value' => $i->cpf,
+
+                        );
+                        echo form_input($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'excluir_'.$id,
+                            'class' => 'checkbox',
+                            'checked' => FALSE
+
+                        );
+                        echo form_checkbox($form_data);
+                        ?>
+                    </th>
+                    <th>
+                        <?php
+                        $form_data = array(
+                            'name' => 'admin_'.$id,
+                            'class' => 'checkbox',
+                            'checked' => $i->permissionLevel == 1 ? TRUE : FALSE
+
+                        );
+                        echo form_checkbox($form_data);
+                        ?>
+                    </th>
+                </tr>
+                <?php
+            }
+        }
+        ?>
+    </table>
+    <?php
+    $buttonData = array(
+        'class' => 'button'
+    );
+    echo form_submit('confirmButton', 'Confirmar Alterações', $buttonData);
+    echo form_close();
+    ?>
 </div>
